eafef1cb22613f8d115ccfb4fd2ce4d9d97042e8
ShehaanN <shehannadeesha2580@gmail.com>
2025-09-24 07:26:12 +0530

    implement product management functions:

---
+++ productController.js
@@ -0,0 +1,41 @@
+import db from "./db.js";
+
+// GET all products
+export const getAllProducts = (callback) => {
+  const sql = "SELECT * FROM products";
+  db.query(sql, (err, result) => {
+    callback(err, result);
+  });
+};
+
+// GET a single product by ID
+export const getProductById = (id, callback) => {
+  const sql = "SELECT * FROM products WHERE id = ?";
+  db.query(sql, [id], (err, result) => {
+    callback(err, result[0]); //The first item from the results array. Since IDs are typically unique, the query should return at most one record
+  });
+};
+
+// CREATE a new product
+export const createProduct = (productData, callback) => {
+  const sql = "INSERT INTO products SET ?";
+  db.query(sql, [productData], (err, result) => {
+    callback(err, result);
+  });
+};
+
+// UPDATE a product
+export const updateProduct = (id, productData, callback) => {
+  const sql = "UPDATE products SET ? WHERE id = ?";
+  db.query(sql, [productData, id], (err, result) => {
+    callback(err, result);
+  });
+};
+
+// DELETE a product
+export const deleteProduct = (id, callback) => {
+  const sql = "DELETE FROM products WHERE id = ?";
+  db.query(sql, [id], (err, result) => {
+    callback(err, result);
+  });
+};
